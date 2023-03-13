6a4b43741986207abd463f7853090778c4fb14c1
JassyAl <jasswalbert@gmail.com>
2023-03-13 00:09:30 -0500

    added index to root

--- api/author/index.php
+++ api/author/index.php
@@ -10,4 +10,26 @@
             exit();
         }
         
+        include_once '../../config/Database.php';
+        include_once '../../models/Author.php';
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Author Search</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Author Search</h1>
+
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
+        <h2 id="enter-a-id">Enter Author ID:</h2>
+		<input type="text" id="author_id" name="author_id"  aria-labelledby="enter-a-city" autofocus required>
+		<button type="submit">Search</button>
+	</form>
+
+</body>
+</html>
