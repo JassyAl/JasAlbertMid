5777197f00b5147f4cb6f32bb9e2b277440868c4
JassyAl <jasswalbert@gmail.com>
2023-03-13 17:45:15 -0500

    edited author read file again

--- api/author/read.php
+++ api/author/read.php
@@ -28,11 +28,15 @@
             extract($row);
   
             // Push to "data"
-            array_push($author_arr, ['id'=>$id, 'author' => $author]);
+            array_push($author_arr, array('id'=>$id, 'author' => $author));
+
+            // array_push($author_arr, ['id'=>$id, 'author' => $author]);
           }
 
         // Turn to JSON & output
-        echo json_encode($author_arr);
+        echo json_encode(array('authors' => $author_arr));
+
+        // echo json_encode($author_arr);
     } else {
         // No Authors
         echo json_encode(
